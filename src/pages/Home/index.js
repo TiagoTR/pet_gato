0285ae800ebf721a9d0a63bd293c8bad42fd6602
Tiago Rocha Martins <tiagorocha.tr16@gmail.com>
2021-06-04 17:51:48 -0300

    Criando a intro da home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,14 @@
-
 import Header from '../../components/Header';
 
+import dog from  '../../assets/icons/cachorro.svg';
+import cat from  '../../assets/icons/gato.svg';
+import hamster from  '../../assets/icons/hamster.svg';
+import rabbit from  '../../assets/icons/coelho.svg';
+import paw from  '../../assets/icons/pe.svg';
+
+
 import './styles.css';
+
 function Home() {
   return (
     <div id="home">
@@ -10,6 +17,14 @@ function Home() {
         <Header/>
         
         <section id="intro">
+          <h2>Melhor lugar pra fazer seu pet feliz</h2>
+          <div className="icons">
+            <img src={dog} alt="cachorro" />
+            <img src={cat} alt="gato"/>
+            <img src={hamster} alt="hamster"/>
+            <img src={rabbit} alt="coelho"/>
+            <img src={paw} alt="pata"/>
+          </div>
         </section>
 
         <section id="Produtos">
